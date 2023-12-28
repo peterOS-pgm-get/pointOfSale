50061dab6e6c5715344e5a9df47a5a5938c44c05
Peter <pcrall04@gmail.com>
2023-12-27 22:34:58 -0800

    Added search to gui

--- gui.lua
+++ gui.lua
@@ -23,16 +23,45 @@ end
 
 gui.window = pos.gui.Window('Point Of Sale')
 pos.gui.addWindow(gui.window)
+gui.window:show()
 
-gui.productList = pos.gui.ListField(1, 2, 32, gui.window.h - 1)
+gui.idSearch = pos.gui.TextInput(1, 2, 4)
+gui.window:addElement(gui.idSearch)
+gui.nameSearch = pos.gui.TextInput(12, 2, 12)
+gui.window:addElement(gui.nameSearch)
+
+gui.productList = pos.gui.ListField(1, 3, 32, gui.window.h - 1)
 gui.window:addElement(gui.productList)
 
+gui.prodEls = {}
 for i, product in pairs(products) do
     local text = ('% 4d % 6s %s'):format(product.id, ('$%.2f'):format(product.price), product.name)
-    local button = pos.gui.Button(1, i, 32, 1, colors.black, nil, text, function()
+    product.button = pos.gui.Button(1, i, 32, 1, colors.black, nil, text, function()
 
     end)
-    gui.productList:addElement(button)
+    gui.productList:addElement(product.button)
+    gui.prodEls[product.id] = product
 end
 
-pos.gui.run()
+local idSearch = ''
+local nameSearch = ''
+pos.gui.run(function(event)
+    if gui.idSearch:getText() ~= idSearch or gui.nameSearch:getText() ~= nameSearch then
+        idSearch = gui.idSearch:getText()
+        for id, product in pairs(products) do
+            local idStr = '' .. id
+            local si, ei = idStr:find(idSearch)
+            if si == 1 or ei == #idStr then
+                product.button.visible = true
+            else
+                product.button.visible = false
+            end
+        end
+        nameSearch = gui.nameSearch:getText()
+        for _, product in pairs(products) do
+            if not product.name:cont(nameSearch) then
+                product.button.visible = false
+            end
+        end
+    end
+end)
